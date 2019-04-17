9bb6c6f3d23aca6a0c8a58dd372e3891afe4de89
L3forge <48441813+L3forge@users.noreply.github.com>
2019-04-17 14:17:03 +0200

    Create testjava.java

---
+++ testjava.java
@@ -0,0 +1,11 @@
+public class RuntimeError
+{
+	public static void main (String[] args)
+	{
+		int taille = 0;
+		int poids = 75;
+		
+		double rapport = poids / taille;
+		System.out.print (rapport);
+	}
+}
