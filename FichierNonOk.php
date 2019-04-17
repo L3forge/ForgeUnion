a79f39021494043b553a15f7a429e0068164984e
L3forge <48441813+L3forge@users.noreply.github.com>
2019-04-17 13:49:15 +0200

    Update FichierNonOk.php

--- FichierNonOk.php
+++ FichierNonOk.php
@@ -10,10 +10,10 @@
     die()
 	}
 	$sql ='SELECT id,prenom,login,statut,age FROM Acces';
-	$req = $db->query($sql);
+	$requesttest = $db->query($sql);
   
 	while ($d = $req->fetch()) 
-	{
+	
 		echo '<b>'.$d['prenom'].' '.$d['login'].'</b> ('.$d['statut'].')';
 		echo ' <i>age : '.$d['age'].'</i> '.' <b><a href="efface.php?id='.$d['id'].'" ><img src="croix.png"></a></b>'.' <b><a href="ajoute.php?id='.$d['id'].'" ><img src="ajoute.png"></b></a><br><br>';
 	}
@@ -27,8 +27,7 @@
 	}
 	if (isset($_GET["msg"]) && $_GET["msg"] == "ajout") {
 		echo 'Ajout ok';
-	}
+	
 
 // Fermeture bd
 $db = null;
-?>
